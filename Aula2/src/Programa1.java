e540f8b5d1bbe217cec637aab785b190e1ec152e
caioagralemos <caioagralemos@live.com>
2023-10-24 20:13:50 -0300

    Renamed Aula2

--- Aula2/src/Programa1.java
+++ Aula2/src/Programa1.java
@@ -4,6 +4,10 @@ public class Programa1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String nome = entrada.nextLine();
-        System.out.println("Olá, " + nome);
+        if (nome.equals("Caio")) {
+            System.out.println("Olá eu!");
+        } else {
+            System.out.println("Olá, " + nome);
+        }
     }
 }
