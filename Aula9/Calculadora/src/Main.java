c2dc93b938c9415ebd46e92f902b42d1776f843f
caioagralemos <caioagralemos@live.com>
2023-11-27 19:23:05 -0300

    Calculadora Pt.3

--- Aula9/Calculadora/src/Main.java
+++ Aula9/Calculadora/src/Main.java
@@ -6,10 +6,14 @@ import java.awt.event.ActionListener;
 public class Main {
     static class Calculator {
         public static String num = "0";
-        public static int memoria = 0;
-        public static String operacao = "+";
+        public static String memoria = "0";
+        public static String operacao = "";
         public static String ButtonPressed(JButton botao) {
-            if (num.equals("0")) {
+            if (num.contains("Resultado")) {
+                memoria = "0";
+                operacao = "";
+                num = botao.getText();
+            } else if (num.equals("0")) {
                 num = botao.getText();
             } else {
                 num = num + botao.getText();
@@ -18,21 +22,40 @@ public class Main {
             return num;
         }
 
+        public static String calcular() {
+            if (operacao.equals("+")) {
+                num = "Resultado: " + (Integer.parseInt(num) + Integer.parseInt(memoria));
+            } else if (operacao.equals("-")) {
+                num = "Resultado: " + (Integer.parseInt(memoria) - Integer.parseInt(num));
+            } else if (operacao.equals("x")) {
+                num = "Resultado: " + (Integer.parseInt(num) * Integer.parseInt(memoria));
+            } else {
+                num = "Resultado: " + (Float.parseFloat(memoria) / Float.parseFloat(num));
+            }
+            return num;
+        }
+
         public static String reset() {
             num = "0";
+            memoria = "0";
+            operacao = "";
             return num;
         }
     }
 
     public static void main(String[] args) {
         JFrame calculadora = new JFrame("Calculadora");
+        calculadora.setLayout(new BorderLayout());
+        calculadora.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calculadora.setSize(300, 500);
 
         JPanel visor = new JPanel();
         visor.add(new JLabel(Calculator.num));
-        visor.setSize(100, 290);
+        visor.setSize(100, 50); // Altura fixa
         visor.setBackground(Color.gray);
-        calculadora.add(visor);
+        calculadora.add(visor, BorderLayout.NORTH);
 
+        JPanel grid = new JPanel(new GridLayout(4,4));
 
         JButton botao_c = new JButton("(C)");
         botao_c.addActionListener(new ActionListener() {
@@ -40,35 +63,112 @@ public class Main {
             public void actionPerformed(ActionEvent e) {
                 Calculator.reset();
                 visor.removeAll();
-                visor.add(new JLabel(Calculator.num));
-                calculadora.revalidate();
+                visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                visor.revalidate();
             }
         });
 
         JButton botao_igual = new JButton("(=)");
+        botao_igual.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!Calculator.operacao.isEmpty()) {
+                    Calculator.calcular();
+                    visor.removeAll();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
+                }
+            }
+        });
+
         JButton botao_adicao = new JButton("(+)");
+        botao_adicao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Calculator.operacao.isEmpty()) {
+                    Calculator.operacao = "+";
+                    Calculator.memoria = Calculator.num;
+                    Calculator.num = "0";
+                    visor.removeAll();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
+                } else {
+                    Calculator.calcular();
+                    visor.revalidate();
+                }
+            }
+        });
+
         JButton botao_subtracao = new JButton("(-)");
+        botao_subtracao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Calculator.operacao.isEmpty()) {
+                    Calculator.operacao = "-";
+                    Calculator.memoria = Calculator.num;
+                    Calculator.num = "0";
+                    visor.removeAll();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
+                } else {
+                    Calculator.calcular();
+                    visor.revalidate();
+                }
+            }
+        });
+
         JButton botao_multiplicacao = new JButton("(x)");
+        botao_multiplicacao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Calculator.operacao.isEmpty()) {
+                    Calculator.operacao = "x";
+                    Calculator.memoria = Calculator.num;
+                    Calculator.num = "0";
+                    visor.removeAll();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
+                } else {
+                    Calculator.calcular();
+                    visor.revalidate();
+                }
+            }
+        });
+
         JButton botao_divisao = new JButton("(/)");
+        botao_divisao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Calculator.operacao.isEmpty()) {
+                    Calculator.operacao = "/";
+                    Calculator.memoria = Calculator.num;
+                    Calculator.num = "0";
+                    visor.removeAll();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
+                } else {
+                    Calculator.calcular();
+                    visor.revalidate();
+                }
+            }
+        });
 
         JButton[] botoes = new JButton[10];
-        botoes[0] = new JButton("0");
-        botoes[1] = new JButton("1");
-        botoes[2] = new JButton("2");
-        botoes[3] = new JButton("3");
-        botoes[4] = new JButton("4");
-        botoes[5] = new JButton("5");
-        botoes[6] = new JButton("6");
-        botoes[7] = new JButton("7");
-        botoes[8] = new JButton("8");
-        botoes[9] = new JButton("9");
+        botoes[0] = new JButton("1");
+        botoes[1] = new JButton("2");
+        botoes[2] = new JButton("3");
+        botoes[3] = new JButton("4");
+        botoes[4] = new JButton("5");
+        botoes[5] = new JButton("6");
+        botoes[6] = new JButton("7");
+        botoes[7] = new JButton("8");
+        botoes[8] = new JButton("9");
+        botoes[9] = new JButton("0");
 
-        calculadora.add(botao_adicao);
-        calculadora.add(botao_subtracao);
-        calculadora.add(botao_multiplicacao);
-        calculadora.add(new JButton());
-        calculadora.add(botao_divisao);
-        calculadora.add(new JButton());
+        grid.add(botao_adicao);
+        grid.add(botao_subtracao);
+        grid.add(botao_multiplicacao);
+        grid.add(botao_divisao);
 
         for (JButton botao : botoes) {
             botao.addActionListener(new ActionListener() {
@@ -76,22 +176,22 @@ public class Main {
                 public void actionPerformed(ActionEvent e) {
                     Calculator.num = Calculator.ButtonPressed(botao);
                     visor.removeAll();
-                    visor.add(new JLabel(Calculator.num));
-                    calculadora.revalidate();
+                    visor.add(new JLabel(Calculator.num), BorderLayout.NORTH);
+                    visor.revalidate();
             }
             });
             if (botao.getText().equals("9")) {
-                calculadora.add(botao_c);
-                calculadora.add(botao);
-                calculadora.add(botao_igual);
+                grid.add(botao_c);
+                grid.add(botao);
+            } else if (botao.getText().equals("0")) {
+                grid.add(botao);
+                grid.add(botao_igual);
             } else {
-                calculadora.add(botao);
+                grid.add(botao);
             }
         }
 
-        calculadora.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calculadora.setSize(300, 500);
-        calculadora.setLayout(new FlowLayout());
+        calculadora.add(grid, BorderLayout.CENTER);
         calculadora.setVisible(true);
     }
 }
